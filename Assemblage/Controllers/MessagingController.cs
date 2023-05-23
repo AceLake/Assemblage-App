cb52bfb438a941d8e4b4ff4c5c67007197ae9b38
Ace Lake <96988100+AceLake@users.noreply.github.com>
2023-05-23 10:36:18 -0600

    I refactored the code to work like it used to. I guess my new version of vs code is wack

---
+++ Assemblage/Controllers/MessagingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assemblage.Controllers
+{
+    public class MessagingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
